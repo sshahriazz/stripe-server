a7b0ec5363dc8787a5e05c312fc75ccba40d5398
Shahriaz Kabir <34005640+sshahriazz@users.noreply.github.com>
2021-11-03 12:28:08 +0600

    server update and nodemon setup

--- server.js
+++ server.js
@@ -1,29 +1,18 @@
-// const express = require('express');
-// eslint-disable-next-line no-undef
 const express = require('express');
 const app = express();
-// This is a sample test API key.
-// eslint-disable-next-line no-undef
 const stripe = require('stripe')('sk_test_DuSG6ZnqiAFAngTiep2WnmsH');
-// eslint-disable-next-line no-undef
 var cors = require('cors');
 
 app.use(express.static('public'));
 app.use(express.json());
-
 app.use(cors());
 
 const calculateOrderAmount = () => {
-  // Replace this constant with a calculation of the order's amount
-  // Calculate the order total on the server to prevent
-  // people from directly manipulating the amount on the client
   return 3000;
 };
-
-app.post('/create-payment-intent', async (req, res) => {
+app.post('/createpi', async (req, res) => {
   const { items } = req.body;
 
-  // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(items),
     currency: 'eur',
@@ -38,11 +27,30 @@ app.post('/create-payment-intent', async (req, res) => {
       'ideal'
     ]
   });
-
   res.send({
     clientSecret: paymentIntent.client_secret,
     amount: paymentIntent.amount
   });
+});
+
+const YOUR_DOMAIN = 'http://localhost:3000/company-information';
+const corsOptions = { origin: '*' };
+
+app.post('/createcs', cors(corsOptions), async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // TODO: replace this with the `price` of the product you want to sell
+        price: 'price_1JrJldEp7iMlLK9IYxXl0pfD',
+        quantity: 5
+      }
+    ],
+    payment_method_types: ['card'],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`
+  });
+  res.send(session.url);
 });
 
 app.listen(4242, () => console.log('Node server listening on port 4242!'));
